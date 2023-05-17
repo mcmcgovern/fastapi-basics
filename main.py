cb22494cebf56ac0f98ab8445d84ce337feff539
Michael McGovern <michaelmcgovern120@gmail.com>
2023-05-17 04:28:12 -0400

    Implement DELETE path with 404 response

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
-from uuid import uuid4
-from fastapi import FastAPI
+from uuid import UUID, uuid4
+from fastapi import FastAPI, HTTPException
 
 from models import Role, User
 
@@ -38,3 +38,15 @@ async def fetch_users():
 async def register_user(user: User):
     db.append(user)
     return {"id": user.id}
+
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return db
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exist"
+    )
